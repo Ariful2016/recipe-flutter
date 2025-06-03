7bbfb8c5fd58bbd6521e5b2ca59e1300acaaccc2
Ariful2016 <arifewucse2016@gmail.com>
2025-06-03 12:52:48 +0600

    implement RegisterState

---
+++ lib/data/models/users/register_state.dart
@@ -0,0 +1,23 @@
+class RegisterState {
+  final bool isLoading;
+  final String? error;
+  final bool isSuccess;
+
+  RegisterState({
+    this.isLoading = false,
+    this.error,
+    this.isSuccess = false,
+  });
+
+  RegisterState copyWith({
+    bool? isLoading,
+    String? error,
+    bool? isSuccess,
+  }) {
+    return RegisterState(
+        isLoading: isLoading ?? this.isLoading,
+        error: error ?? this.error,
+        isSuccess: isSuccess ?? this.isSuccess
+    );
+  }
+}
